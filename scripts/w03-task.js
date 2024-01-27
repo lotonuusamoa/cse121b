4c5bac7e408069a2a361c798ebd950ac2e6cde02
lotonuusamoa <andre@lotonuu.com>
2024-01-27 12:49:56 -1100

    W03

---
+++ scripts/w03-task.js
@@ -0,0 +1,34 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+function add (number1, number2){
+    return number1 + number2;
+}
+let addNumber1 = Number(document.querySelector('#add').value);
+/* Function Definition - Add Numbers */
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
+document.querySelector('#addNumbers').addEventListener('click', addNumber);
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
